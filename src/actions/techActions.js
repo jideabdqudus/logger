395950c9e5ca88e91d7d36842b75acad2be35d65
jideabdqudus <jideabdqudus@gmail.com>
2020-03-17 13:00:53 +0100

    App ready for use :)

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -8,73 +8,71 @@ import {
 
 //Get techs from server
 export const getTechs = () => async (dispatch) => {
-    try {
-      setLoading();
-  
-      const res = await fetch("/techs");
-      const data = await res.json();
-  
-      dispatch({
-        type: GET_TECHS,
-        payload: data
-      });
-    } catch (error) {
-      dispatch({
-        type: TECHS_ERROR,
-        payload: error.response.statusText
-      });
-    }
-  };
+  try {
+    setLoading();
 
+    const res = await fetch("/techs");
+    const data = await res.json();
 
-  //Add tech to server
+    dispatch({
+      type: GET_TECHS,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText
+    });
+  }
+};
 
-  export const addTech = (tech) => async (dispatch) => {
-    try {
-      setLoading();
-  
-      const res = await fetch("/techs",{
-        method: 'POST',
-        body: JSON.stringify(tech),
-        headers:{
-          'Content-Type': 'application/json'
-        }
-      })
-      const data = await res.json();
+//Add tech to server
 
-      dispatch ({
-        type: ADD_TECH,
-        payload: data
-      });
-    } catch (error) {
-      dispatch({
-        type: TECHS_ERROR,
-        payload: error.response.statusText
-      });
-    }
-  };
+export const addTech = (tech) => async (dispatch) => {
+  try {
+    setLoading();
 
+    const res = await fetch("/techs", {
+      method: "POST",
+      body: JSON.stringify(tech),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const data = await res.json();
 
-  //Delete Tech
+    dispatch({
+      type: ADD_TECH,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText
+    });
+  }
+};
 
-  export const deleteTech = (id) => async (dispatch) => {
-    try {
-      setLoading();
-      
-      await fetch(`/techs/${id}`, {
-        mthod: "DELETE"
-      })
-      dispatch({
-        type: DELETE_TECH,
-        payload: id
-      });
-    } catch (error) {
-      dispatch({
-        type: TECHS_ERROR,
-        payload: error.response.statusText
-      });
-    }
-  };
+//Delete Tech
+
+export const deleteTech = (id) => async (dispatch) => {
+  try {
+    setLoading();
+
+    await fetch(`/techs/${id}`, {
+      mthod: "DELETE"
+    });
+    dispatch({
+      type: DELETE_TECH,
+      payload: id
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText
+    });
+  }
+};
 
 //Set loading to true
 export const setLoading = () => {
--- src/components/techs/TechItem.js
+++ src/components/techs/TechItem.js
@@ -2,24 +2,19 @@ import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { deleteTech } from "../../actions/techActions";
-import M from 'materialize-css/dist/js/materialize.min.js'
-
+import M from "materialize-css/dist/js/materialize.min.js";
 
 const TechItem = ({ tech: { id, firstName, lastName }, deleteTech }) => {
-  const onDelete=()=>{
-      deleteTech(id)
-      M.toast({html:'Tech Deleted'})
-  }
-  
-    return (
+  const onDelete = () => {
+    deleteTech(id);
+    M.toast({ html: "Tech Deleted" });
+  };
+
+  return (
     <li className="collection-item">
       <div>
         {firstName} {lastName}
-        <a
-          href="#!"
-          className="secondary-content"
-          onClick={onDelete}
-        >
+        <a href="#!" className="secondary-content" onClick={onDelete}>
           <i className="material-icons grey-text">delete</i>
         </a>
       </div>
