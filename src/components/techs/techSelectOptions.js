1d0d3fa97c047fb9b37206b5f879c7aa8a2a953e
jideabdqudus <jideabdqudus@gmail.com>
2020-03-17 11:50:19 +0100

    New Additions

--- src/components/techs/techSelectOptions.js
+++ src/components/techs/techSelectOptions.js
@@ -1,23 +1,32 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {getTechs} from '../../actions/techActions'
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getTechs } from "../../actions/techActions";
 
-const techSelectOptions = ({getTechs, tech: {techs, loading}}) => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const techSelectOptions = ({ getTechs, tech: { techs, loading } }) => {
+  useEffect(() => {
+    getTechs();
+    //eslint-disable-next-line
+  }, []);
+
+  return (
+    !loading &&
+    techs !== null &&
+    techs.map((tech) => (
+      <option key={t.id} value={`${t.firstName} ${t.lastName}`}>
+        {t.firstName} {t.lastName}
+      </option>
+    ))
+  );
+};
 
 techSelectOptions.propTypes = {
-tech: PropTypes.object.isRequired,
-getTechs: PropTypes.func.isRequired
-}
+  tech: PropTypes.object.isRequired,
+  getTechs: PropTypes.func.isRequired
+};
 
-const mapStateToProps = state =>({
-    tech:state.tech
-})
+const mapStateToProps = (state) => ({
+  tech: state.tech
+});
 
-export default techSelectOptions
+export default connect(mapStateToProps, { getTechs })(techSelectOptions);
